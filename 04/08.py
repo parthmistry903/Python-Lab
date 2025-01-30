2f67ed3539fb3efb37dc347f07e32bb95a54b9fd
parthmistry903 <parthmistry903@gmail.com>
2025-01-30 15:42:13 +0530

    updated

--- 04/08.py
+++ 04/08.py
@@ -1,6 +1,7 @@
 # 8. Print factorial of a given number.
+from math import factorial
+
 def fact():
-    from math import factorial
     num = int(input("Enter a number: "))
     print(f"Factorial of {num} is {factorial(num)}")
 
